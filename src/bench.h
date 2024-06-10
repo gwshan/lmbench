90c01bb7e33ef33c94f6d76ba402cf6064310c6f
Gavin Shan <gshan@redhat.com>
2024-06-11 09:39:42 +1000

    Fix build error on ARM64 server
    Signed-off-by: Gavin Shan <gshan@redhat.com>

--- src/bench.h
+++ src/bench.h
@@ -320,4 +320,6 @@ extern int handle_scheduler(int childno, int benchproc, int nbenchprocs);
 extern char *rpc_xact_1();
 extern char *client_rpc_xact_1();
 
+extern void lmbench_usage(int argc, char *argv[], char* usage);
+
 #endif /* _BENCH_H */
